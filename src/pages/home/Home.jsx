a4077d1184edf8aac2d1bec6f38948bedb34a5a5
powerscript <jameskagunga2022@gmail.com>
2022-05-04 11:50:15 +0300

    adding files

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import "./home.css";
+import Navbar from "../../components/navbar/Navbar";
+
+const home = () => {
+    return (
+        <div>
+
+            <Navbar />
+            
+        </div>
+    )
+}
+
+export default home
---
+++ src/pages/list/List.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const List = () => {
+    return (
+        <div>
+            
+            List
+        </div>
+    )
+}
+
+export default List
